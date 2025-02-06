04a62c390634ab6e3e09a8e4a52fb7c3c32749b7
LC2020429 <lcordova-2020429@kinal.edu.gt>
2025-02-06 15:56:30 +0000

    Actualización de modelos

--- src/curse/curse.model.js
+++ src/curse/curse.model.js
@@ -1,22 +1,26 @@
 import { Schema } from "mongoose";
 const curseSchema = Schema({
-    curseName: {
-      type: String,
-      required: [true, "Name collage is requierd"],
-      maxLength: [30, "Name collage can not exced 30 characters"],
-    },
-    curseDescription: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    curseTeacher: {
-      type: String,
-      required: [true, "Password is required"],
-    },
-    curseStatus: {
-      type: Boolean,
-      default: true,
-    }
-  });
-  
+  curseName: {
+    type: String,
+    required: [true, "Name curse is requierd"],
+    maxLength: [30, "Name curse can not exced 30 characters"],
+  },
+  curseDescription: {
+    type: String,
+    required: [true, "curse description is required"],
+    maxLength: [30, "description can not exced 30 characters"],
+    unique: true,
+  },
+  curseTeacher: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  fullQuota: {
+    type: Number,
+    required: [true, "This course need a max of students"]
+  },
+  curseStatus: {
+    type: Boolean,
+    default: true,
+  }  
+});
